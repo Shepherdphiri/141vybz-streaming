19fce0ae5d008a444b9e49a0515fcac138f893f7
Shepherd Zisper Phiri <155381762+Shepherdphiri@users.noreply.github.com>
2025-06-25 16:14:00 +0200

    Updated server.js

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server, path: '/broadcast' });
+
+const broadcastWSS = new WebSocket.Server({ noServer: true });
+const streamWSS = new WebSocket.Server({ noServer: true });
 
 let clients = [];
 
-wss.on('connection', (ws) => {
+// Handle broadcaster connection
+broadcastWSS.on('connection', (ws) => {
   console.log('Broadcaster connected');
   ws.on('message', (data) => {
     clients.forEach((client) => {
@@ -22,8 +25,15 @@ wss.on('connection', (ws) => {
   ws.on('close', () => console.log('Broadcaster disconnected'));
 });
 
-const streamWSS = new WebSocket.Server({ noServer: true });
+// Handle listener connection
+streamWSS.on('connection', (ws) => {
+  clients.push(ws);
+  ws.on('close', () => {
+    clients = clients.filter((c) => c !== ws);
+  });
+});
 
+// Serve HTML pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -32,22 +42,22 @@ app.get('/broadcast', (req, res) => {
   res.sendFile(path.join(__dirname, 'broadcast.html'));
 });
 
+app.use(express.static(__dirname));
+
+// Upgrade logic
 server.on('upgrade', (req, socket, head) => {
-  if (req.url === '/stream') {
+  if (req.url === '/broadcast') {
+    broadcastWSS.handleUpgrade(req, socket, head, (ws) => {
+      broadcastWSS.emit('connection', ws, req);
+    });
+  } else if (req.url === '/stream') {
     streamWSS.handleUpgrade(req, socket, head, (ws) => {
-      clients.push(ws);
-      ws.on('close', () => {
-        clients = clients.filter((c) => c !== ws);
-      });
+      streamWSS.emit('connection', ws, req);
     });
   } else {
-    wss.handleUpgrade(req, socket, head, (ws) => {
-      wss.emit('connection', ws, req);
-    });
+    socket.destroy();
   }
 });
-
-app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
